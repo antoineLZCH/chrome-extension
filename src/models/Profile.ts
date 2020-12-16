2d2791811bd23051cc1fd133197458b3a67fe140
Antoine LOZACH <antoinelozach.pro@gmail.com>
2020-12-16 15:21:20 +0100

    📦 NEW: Initial Commit

---
+++ src/models/Profile.ts
@@ -0,0 +1,6 @@
+export default interface Profile {
+  fullName: string,
+  title: string,
+  country: string,
+  imageSrc: string
+}
--- src/scripts/linkedin.ts
+++ src/scripts/linkedin.ts
@@ -1,10 +1,27 @@
-import { Result } from "../models/result";
+import Profile from "../models/Profile";
+
+async function getProfileInfos(): Promise<Profile> {
+  const fullName = document.querySelectorAll('.pv-top-card--list li')[0];
+  const title = document.querySelectorAll('.pv-top-card h2.mt1')[0];
+  const image = document.querySelectorAll('.profile-photo-edit img')[0]
+  return {
+    fullName: fullName.textContent.replace(/\n/, "").trim(),
+    title: title.textContent.replace(/\n/, "").trim(),
+    country: "XXX",
+    imageSrc: image.getAttribute('src')
+  };
+}
+
+let profile: Profile = null;
+setTimeout(() => {
+  getProfileInfos().then(result => {
+    profile = result || profile;
+  })
+}, 3000)
 
 chrome.runtime.onMessage.addListener(async (msg, sender, response) => {
-  const result: Result = {
-    message: "Hello"
-  };
-
-  response(result.message);
+  if (msg.from === "popup" && msg.subject === "getFullName") {
+    response(profile)
+  }
   return true;
 });
